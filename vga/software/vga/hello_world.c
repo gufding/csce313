10552339fff9f52c13232fac63582769f1b3f6e0
gufding <maxjhensler@gmail.com>
2020-02-18 17:34:02 -0500

    somewhat working display rotation

--- vga/software/vga/hello_world.c
+++ vga/software/vga/hello_world.c
@@ -9,6 +9,8 @@
 #include <sys/alt_alarm.h>
 #include "myfile.h"
 
+#define PI 3.141592
+
 void measuringFrameRate() {
   alt_u32 ticks_per_second = alt_ticks_per_second();
   alt_u32 current_tick = 0, prev_tick = alt_nticks();
@@ -20,48 +22,72 @@ void displayFrameRate(alt_up_char_buffer_dev *char_buf) {
   alt_up_char_buffer_string(char_buf, "Video Works", 35, 50);
 }
 
-void displayImage (alt_u8 *image_array, int screen_width, int screen_height) {
+void displayImage (alt_u8 *image_array, int screen_width, int screen_height, float angle) {
   alt_up_pixel_buffer_dma_dev *pix_buf;
   pix_buf = alt_up_pixel_buffer_dma_open_dev("/dev/video_pixel_buffer_dma_0");
   alt_up_pixel_buffer_dma_clear_screen(pix_buf, 0);
-
-  // Working for loop to display on the screen
-  /*for (int i = 0; i < screen_height; i++) {
-    for (int j = 0; j < screen_width; j++) {
-      alt_up_pixel_buffer_dma_draw(pix_buf,
-       (myimage[(i*screen_height+j)*3+0] << 16) +
-       (myimage[(i*screen_height+j)*3+1]<<8) + 
-       (myimage[(i*screen_height+j)*3+2]), j, i);
-    }
-  }*/
   
-  // Attempting to rotate the image 30 degrees. This is not working at the moment
+  // Display the image at the angle given
+  alt_u16 r;
+  alt_u16 c;
   for (int i = 0; i < screen_height; i++) {
     for (int j = 0; j < screen_width; j++) {
-      r = i-160;
-      c = j-240;
-      r = (j-240)*(-sin(30))+(i-160)*cos(30);
-      c = (j-240)*cos(30)+(i-160)*sin(30);
-      r += 160;
-      c += 240;
+      // Get the translated image by coordinates for row and colmn
+      r = ((j-160)*sin(angle))+((i-120)*cos(angle));
+      c = ((j-160)*cos(angle))+((i-120)*(-sin(angle)));
+      // Get the coordinates back to top left of screen
+      r += 120;
+      c += 160;
       r = roundf(r);
       c = roundf(c);
-      
+   
+      // Draw the image
       alt_up_pixel_buffer_dma_draw(pix_buf,
        (myimage[(i*screen_height+j)*3+0] << 16) +
        (myimage[(i*screen_height+j)*3+1] << 8) + 
        (myimage[(i*screen_height+j)*3+2]), (int)c, (int)r);
     }
-  }
-  
-  
+  }  
 }
 
 int main() {  
   myimage[230401];
   alt_up_char_buffer_dev *char_buf;
-  displayImage(myimage, 480, 320);
+  float angles[12] = {0, PI/6, PI/3, PI/2, (2*PI)/3, (5*PI)/6, PI,
+                     (7*PI)/6, (4*PI)/3, (3*PI)/2, (5*PI)/3, (11*PI)/6, 2*PI};
+  while(1) {
+    for (int i = 0; i < 13; i++) {
+      displayImage(myimage, 480, 320, angles[i]);
+    }
+    for (int i = 13; i != 0; i--) {
+      displayImage(myimage, 480, 320, angles[i]);
+    }
+  }
   displayFrameRate(char_buf);
     
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
